a91b4f1de9d3baaac93cf6aa16764ca9ece2a02e
Pratap Rathi <prataprathi97@gmail.com>
2024-03-10 16:05:35 +0530

    Added create user functionality by making postgree connection functional using prisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,32 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { RegisterSchema } from '@/schemas/index';
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
-    if(!validateFields.success){
-        return {error: "Fields are Invalid!"}
+    if (!validateFields.success) {
+        return { error: "Fields are Invalid!" }
     }
-    
-    return {success: "Email sent successfully"}
+    const { name, email, password } = validateFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return { error: "Email already in use" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+
+    // TODO: send verfication token to email
+
+    return { success: "User Created" }
 }
